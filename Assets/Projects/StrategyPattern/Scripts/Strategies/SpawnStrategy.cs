7598d2091aa8979c495941723278979fcb858bb8
Jason Skillman <jason_skillman@outlook.com>
2025-05-30 21:38:01 -0400

    Create strategy pattern

---
+++ Assets/Projects/StrategyPattern/Scripts/Strategies/SpawnStrategy.cs
@@ -0,0 +1,10 @@
+﻿namespace StrategyPattern.Strategies
+{
+	using UnityEngine;
+
+	[CreateAssetMenu(fileName = "SpawnStrategy", menuName = "Strategy Pattern/Spawn Strategy", order = 0)]
+	public class SpawnStrategy : ScriptableObject
+	{
+		public virtual Vector3 SetPosition(Vector3 origin) => origin;
+	}
+}
